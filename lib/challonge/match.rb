6cc70626581a94589195155362ce42dd538dae85
Emil Palm <emil@x86.nu>
2011-06-30 14:55:29 +0200

    Adding support for getting the players from a matchup

--- lib/challonge/match.rb
+++ lib/challonge/match.rb
@@ -10,6 +10,18 @@ class Challonge::Match < Challonge::API
   def tournament=(tournament)
     self.prefix_options[:tournament_id] = tournament.id
   end
+  
+  def player1
+    _find_player(:player1_id)
+  end
+  
+  def player2
+    _find_player(:player2_id)
+  end
+  
+  def player_winner? (participant)
+    (participant.id != self.winner_id)
+  end
 
   # not implemented by API
   def create; end
@@ -21,5 +33,14 @@ class Challonge::Match < Challonge::API
 
   def readonly_attributes
     %w/prerequisite_match_ids_csv/
+  end
+  
+  private
+  def _find_player(player)
+    if self.attributes[player] != nil
+      Challonge::Participant.find(self.attributes[player], :params => {:tournament_id => self.prefix_options[:tournament_id]})
+    else
+      Challonge::Participant.new({:name => 'TBD'})
+    end
   end
 end
